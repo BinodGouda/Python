9821763f57dc9a1d89f9bbc2b4b149f156e4c5f0
BinodGouda <goudabinod2003@gmail.com>
2025-02-28 22:07:55 +0530

    Add Ecersice_01

--- Excersice_01.py
+++ Excersice_01.py
@@ -15,3 +15,13 @@ elif(operator == '*'):
 elif(operator == '/'):
     sum = num1 / num2
     print(f"Sum of {num1} and {num2} = {sum}")
+elif(operator == '//'):
+    sum = num1 // num2
+    print(f"Sum of {num1} and {num2} = {sum}")
+elif(operator == '**'):
+    sum = num1 ** num2
+    print(f"Sum of {num1} and {num2} = {sum}")
+elif(operator == '%'):
+    sum = num1 % num2
+    print(f"Sum of {num1} and {num2} = {sum}")
+
