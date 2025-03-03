4816487a68a17e94347e7dfcf2928e1e2acd59d3
BinodGouda <goudabinod2003@gmail.com>
2025-03-03 22:19:18 +0530

    Excersice 2

---
+++ Excersice_2.py
@@ -0,0 +1,18 @@
+import time
+
+timestamp = int(time.strftime("%H"))
+
+if (timestamp >= 4 and timestamp < 12):
+    print("Good Morning")
+
+elif(timestamp >= 12 and timestamp < 18):
+    print("Good Afternoon")
+
+elif(timestamp >= 18 and timestamp < 20):
+    print("Good Evning")
+
+elif(timestamp >= 20 and timestamp < 24):
+    print("Good Night")
+
+else:
+    print("Invalid Time")
